1854b23ebb9b5b6ebc3c49e0d43ef541a727d69e
Crash105 <bmm49@njit.edu>
2025-09-02 00:35:32 -0400

    first changes

--- Beginner/src/TodoListManager.java
+++ Beginner/src/TodoListManager.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
-public class TodoListManager {
-    private ArrayList<Task> tasks;
+public class TodoListManager<T> {
+    private ArrayList<T> tasks;
 
     public TodoListManager() {
         tasks = new ArrayList<>();
     }
 
-    public void addTask(Task task) {
+    public void addTask(T task) {
         tasks.add(task);
         System.out.println("Task added: " + task);
     }
@@ -23,11 +23,20 @@ public class TodoListManager {
     }
 
     public void removeTask(int index) {
-        if (index >= 0 && index < tasks.size()) {
-            Task removedTask = tasks.remove(index);
-            System.out.println("Task removed: " + removedTask);
+        if (index >= 1 && index <= tasks.size()) {
+            System.out.println("Task removed: " + tasks.get(index - 1));
+            tasks.remove(index - 1);
         } else {
-            System.out.println("Task index out of range.");
+            System.out.println("Invalid task number.");
+        }
+    }
+
+    public void completeTask(int index) {
+        if (index >= 1 && index <= tasks.size()) {
+            System.out.println("Task completed: " + tasks.get(index - 1));
+            tasks.remove(index - 1);
+        } else {
+            System.out.println("Invalid task number.");
         }
     }
 }
